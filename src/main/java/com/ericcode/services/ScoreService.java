e69c2ad91ac800c01b3de913e0191cdc1e75e134
Eric Reisenbuckler <ericreisenbuckler@Erics-MacBook-Pro.local>
2023-02-19 22:56:52 -0500

    Imported Spring Boot and created ScoreService

--- src/main/java/com/ericcode/services/ScoreService.java
+++ src/main/java/com/ericcode/services/ScoreService.java
@@ -1,5 +1,11 @@
 package com.ericcode.services;
 
+
+import org.springframework.web.client.RestTemplate;
+
 public class ScoreService {
-    
+
+    private static final String API_BASE_URL = "https://api-nba-v1.p.rapidapi.com/";
+
+    private final RestTemplate restTemplate = new RestTemplate();
 }
